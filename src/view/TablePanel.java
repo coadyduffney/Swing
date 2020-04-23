90b38c3224ecbcbe656666e8ecf5b3337021863e
Coady Duffney <cduffney@protocase.com>
2020-04-23 15:09:28 -0300

    Popup Menus

--- src/view/TablePanel.java
+++ src/view/TablePanel.java
@@ -6,8 +6,12 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.List;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import model.Person;
@@ -20,12 +24,27 @@ public class TablePanel extends JPanel {
 
     private JTable table;
     private PersonTableModel tableModel;
+    private JPopupMenu popup;
     
     
     public TablePanel() {
         
         tableModel = new PersonTableModel();
         table = new JTable(tableModel);
+        popup = new JPopupMenu();
+        
+        JMenuItem removeItem = new JMenuItem("Delete Row");
+        popup.add(removeItem);
+        
+        table.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    popup.show(table, e.getX(), e.getY());
+                }
+            }
+            
+        });
         
         setLayout(new BorderLayout());
         
